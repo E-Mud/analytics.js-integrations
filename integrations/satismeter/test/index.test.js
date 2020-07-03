8194209d86bda1159305586e02e2647c790ef3b2
E-Mud <better.acs@gmail.com>
2020-07-03 11:32:31 +0200

    Refactored SatisMeter tests

--- integrations/satismeter/test/index.test.js
+++ integrations/satismeter/test/index.test.js
@@ -6,16 +6,13 @@ var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
 var SatisMeter = require('../lib/');
 
-describe('SatisMeter', function() {
+function testSuite(satismeterOptions, writeKey) {
   var analytics;
   var satismeter;
-  var options = {
-    apiKey: 'xy1gopRgdl'
-  };
 
   beforeEach(function() {
     analytics = new Analytics();
-    satismeter = new SatisMeter(options);
+    satismeter = new SatisMeter(satismeterOptions);
     analytics.use(SatisMeter);
     analytics.use(tester);
     analytics.add(satismeter);
@@ -70,7 +67,7 @@ describe('SatisMeter', function() {
       it('should send apiKey and user id', function() {
         analytics.identify('id');
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           traits: {},
           type: 'identify'
@@ -80,7 +77,7 @@ describe('SatisMeter', function() {
       it('should send email', function() {
         analytics.identify('id', { email: 'email@example.com' });
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           traits: {
             email: 'email@example.com'
@@ -92,7 +89,7 @@ describe('SatisMeter', function() {
       it('should send user name', function() {
         analytics.identify('id', { name: 'john doe' });
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           traits: {
             name: 'john doe'
@@ -105,7 +102,7 @@ describe('SatisMeter', function() {
         var now = new Date();
         analytics.identify('id', { createdAt: now });
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           traits: {
             createdAt: now
@@ -122,7 +119,7 @@ describe('SatisMeter', function() {
           language: 'en'
         });
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           traits: {
             translation: {
@@ -169,7 +166,7 @@ describe('SatisMeter', function() {
         });
 
         analytics.called(window.satismeter, {
-          writeKey: options.apiKey,
+          writeKey: writeKey,
           userId: 'id',
           type: 'page',
           page: {
@@ -185,164 +182,20 @@ describe('SatisMeter', function() {
       });
     });
   });
+}
+
+describe('SatisMeter', function() {
+  var options = {
+    apiKey: 'xy1gopRgdl'
+  };
+
+  testSuite(options, options.apiKey);
 });
 
 describe('SatisMeter - legacy setup', function() {
-  // to be removed once we complete the renaming from token to apiKey
-  var analytics;
-  var satismeter;
   var options = {
     token: 'xy1gopRgdl'
   };
 
-  beforeEach(function() {
-    analytics = new Analytics();
-    satismeter = new SatisMeter(options);
-    analytics.use(SatisMeter);
-    analytics.use(tester);
-    analytics.add(satismeter);
-  });
-
-  afterEach(function() {
-    analytics.restore();
-    analytics.reset();
-    satismeter.reset();
-    sandbox();
-  });
-
-  it('should have the right settings', function() {
-    analytics.compare(
-      SatisMeter,
-      integration('SatisMeter')
-        .global('satismeter')
-        .option('token', '')
-    );
-  });
-
-  describe('before loading', function() {
-    beforeEach(function() {
-      analytics.stub(satismeter, 'load');
-    });
-
-    describe('#initialize', function() {
-      it('should call #load', function() {
-        analytics.initialize();
-        analytics.called(satismeter.load);
-      });
-    });
-  });
-
-  describe('loading', function() {
-    it('should load', function(done) {
-      analytics.load(satismeter, done);
-    });
-  });
-
-  describe('after loading', function() {
-    beforeEach(function(done) {
-      analytics.once('ready', done);
-      analytics.initialize();
-    });
-
-    describe('#identify', function() {
-      beforeEach(function() {
-        analytics.stub(window, 'satismeter');
-      });
-
-      it('should send token and user id', function() {
-        analytics.identify('id');
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          traits: {},
-          type: 'identify'
-        });
-      });
-
-      it('should send email', function() {
-        analytics.identify('id', { email: 'email@example.com' });
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          traits: {
-            email: 'email@example.com'
-          },
-          type: 'identify'
-        });
-      });
-
-      it('should send user name', function() {
-        analytics.identify('id', { name: 'john doe' });
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          traits: {
-            name: 'john doe'
-          },
-          type: 'identify'
-        });
-      });
-
-      it('should send signUpDate', function() {
-        var now = new Date();
-        analytics.identify('id', { createdAt: now });
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          traits: {
-            createdAt: now
-          },
-          type: 'identify'
-        });
-      });
-
-      it('should send custom traits', function() {
-        analytics.identify('id', {
-          translation: {
-            FOLLOWUP: 'What can we improve'
-          },
-          language: 'en'
-        });
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          traits: {
-            translation: {
-              FOLLOWUP: 'What can we improve'
-            },
-            language: 'en'
-          },
-          type: 'identify'
-        });
-      });
-    });
-
-    describe('#page', function() {
-      beforeEach(function() {
-        analytics.stub(window, 'satismeter');
-      });
-
-      it('should send apiKey, user id and page properties', function() {
-        analytics.user().id('id');
-        analytics.page('Pricing', {
-          customProperty: 'Example'
-        });
-
-        analytics.called(window.satismeter, {
-          writeKey: options.token,
-          userId: 'id',
-          type: 'page',
-          page: {
-            name: 'Pricing',
-            path: window.location.pathname,
-            referrer: window.document.referrer,
-            search: window.location.search,
-            title: window.document.title,
-            url: window.location.href,
-            customProperty: 'Example'
-          }
-        });
-      });
-    });
-  });
+  testSuite(options, options.token);
 });
